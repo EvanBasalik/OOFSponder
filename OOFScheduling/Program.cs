403ed90d7cfbd71d4d3c8207cad455f2b353f1ac
Evan Basalik <evanba@microsoft.com>
2017-11-18 21:50:57 -0500

    Add global error handler

--- OOFScheduling/Program.cs
+++ OOFScheduling/Program.cs
@@ -20,46 +20,57 @@ namespace OOFScheduling
         static void Main()
         {
 
-            //http://covingtoninnovations.com/mc/SingleInstance.html
-            bool gotMutex;
-            //GUID is generated using the built-in VS capability
-            System.Threading.Mutex m = new System.Threading.Mutex(true, "{6FE49292-F7B3-4EB7-B8F2-0CDDFE20B737}", out gotMutex);
-
-            //http://covingtoninnovations.com/mc/SingleInstance.html
-            if (gotMutex)
+            try
             {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new Form1());
-                GC.KeepAlive(m);
-            }
-            else
-            {
-                //http://www.codebetter.com/paullaudeman/2004/07/17/windows-forms-tip-ensure-only-one-instance-of-your-application-is-running-at-a-time/
-                // see if we can find the other app and Bring it to front
-                //NOTE: the link referenced for this specifies a string for the first value, but
-                //I ended up being able to just use the Window name
-                IntPtr hWnd = NativeMethods.FindWindow(null, "OOFSponder");
+                //http://covingtoninnovations.com/mc/SingleInstance.html
+                bool gotMutex;
+                //GUID is generated using the built-in VS capability
+                System.Threading.Mutex m = new System.Threading.Mutex(true, "{6FE49292-F7B3-4EB7-B8F2-0CDDFE20B737}", out gotMutex);
 
-                if (hWnd != IntPtr.Zero)
+                //http://covingtoninnovations.com/mc/SingleInstance.html
+                if (gotMutex)
                 {
-                    NativeMethods.WINDOWPLACEMENT placement = new NativeMethods.WINDOWPLACEMENT();
-                    placement.length = System.Runtime.InteropServices.Marshal.SizeOf(placement);
-
-                    NativeMethods.GetWindowPlacement(hWnd, ref placement);
-
-                    if (placement.showCmd != NativeMethods.SW_MINIMIZE)
-                    {
-                        placement.showCmd = NativeMethods.SW_RESTORE;
-
-                        NativeMethods.SetWindowPlacement(hWnd, ref placement);
-                        NativeMethods.SetForegroundWindow(hWnd);
-                    }
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new Form1());
+                    GC.KeepAlive(m);
                 }
+                else
+                {
+                    //http://www.codebetter.com/paullaudeman/2004/07/17/windows-forms-tip-ensure-only-one-instance-of-your-application-is-running-at-a-time/
+                    // see if we can find the other app and Bring it to front
+                    //NOTE: the link referenced for this specifies a string for the first value, but
+                    //I ended up being able to just use the Window name
+                    IntPtr hWnd = NativeMethods.FindWindow(null, "OOFSponder");
 
-                return;
+                    if (hWnd != IntPtr.Zero)
+                    {
+                        NativeMethods.WINDOWPLACEMENT placement = new NativeMethods.WINDOWPLACEMENT();
+                        placement.length = System.Runtime.InteropServices.Marshal.SizeOf(placement);
 
+                        NativeMethods.GetWindowPlacement(hWnd, ref placement);
+
+                        if (placement.showCmd != NativeMethods.SW_MINIMIZE)
+                        {
+                            placement.showCmd = NativeMethods.SW_RESTORE;
+
+                            NativeMethods.SetWindowPlacement(hWnd, ref placement);
+                            NativeMethods.SetForegroundWindow(hWnd);
+                        }
+                    }
+
+                    return;
+
+                }
             }
+            catch (Exception ex)
+            {
+                OOFSponderInsights.TrackException("Fatal error on startup", ex);
+                MessageBox.Show("Uh oh! Fatal error for OOFSponder. Please try again.");
+                return;
+            }
+
+ 
         }
 
         
