bf20fb4733d5aef99778d5a940243d8dbda38adf
EvanBasalik <evanba@microsoft.com>
2021-01-01 09:14:46 -0500

    add critical section for auth code, added AI logging to base OOFSponderLogger class

--- OOFScheduling/Logger.cs
+++ OOFScheduling/Logger.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OOFScheduling;
+using System;
 using System.Diagnostics;
 
 namespace OOFSponder
@@ -33,6 +34,14 @@ namespace OOFSponder
                                   type,
                                   module,
                                   message));
+
+            //also send everything to AppInsights
+            OOFSponderInsights.Track(
+                    string.Format("{0},{1},{2}",
+                                  type,
+                                  module,
+                                  message)
+                );
         }
     }
 }
