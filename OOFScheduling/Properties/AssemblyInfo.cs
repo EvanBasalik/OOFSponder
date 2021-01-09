74929dc7c4fb3c8217fd3144611207fec247f1ec
EvanBasalik <evanba@microsoft.com>
2021-01-09 18:55:13 -0500

    1.1.5.0 Insider release

--- OOFScheduling/Properties/AssemblyInfo.cs
+++ OOFScheduling/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 <<<<<<< HEAD
-[assembly: AssemblyVersion("1.1.4.0")]
-[assembly: AssemblyFileVersion("1.1.4.0")]
+[assembly: AssemblyVersion("1.1.5.0")]
+[assembly: AssemblyFileVersion("1.1.5.0")]
 =======
 [assembly: AssemblyVersion("1.1.2.5")]
 [assembly: AssemblyFileVersion("1.1.2.5")]
