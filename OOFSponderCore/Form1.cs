4f5a89e2dfbd47abfaa84a76a7289e2d9b857339
Evan Basalik <evanba@microsoft.com>
2024-12-06 11:39:22 -0500

    Copy over core logic, move extensions to separate class

--- OOFSponderCore/Form1.cs
+++ OOFSponderCore/Form1.cs
@@ -1473,34 +1473,34 @@ namespace OOFScheduling
     }
 
 
-    internal static class Extensions
-    {
-        internal static string CleanReplyMessage(this string input)
-        {
-            return Regex.Replace(input, @"\r\n|\n\r|\n|\r", "\r\n");
-        }
+    //internal static class Extensions
+    //{
+    //    internal static string CleanReplyMessage(this string input)
+    //    {
+    //        return Regex.Replace(input, @"\r\n|\n\r|\n|\r", "\r\n");
+    //    }
 
-        public static T GetPrivateField<T>(this object obj, string name)
-        {
-            BindingFlags flags = BindingFlags.Instance | BindingFlags.NonPublic;
-            Type type = obj.GetType();
-            FieldInfo field = type.GetField(name, flags);
-            return (T)field.GetValue(obj);
-        }
+    //    public static T GetPrivateField<T>(this object obj, string name)
+    //    {
+    //        BindingFlags flags = BindingFlags.Instance | BindingFlags.NonPublic;
+    //        Type type = obj.GetType();
+    //        FieldInfo field = type.GetField(name, flags);
+    //        return (T)field.GetValue(obj);
+    //    }
 
-    }
+    //}
 
-    public static class StringExtensions
-    {
-        internal static string FirstCharToUpper(this string input)
-        {
-            switch (input)
-            {
-                case null: throw new ArgumentNullException(nameof(input));
-                case "": throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input));
-                default: return input[0].ToString().ToUpper() + input.Substring(1);
-            }
-        }
-    }
+    //public static class StringExtensions
+    //{
+    //    internal static string FirstCharToUpper(this string input)
+    //    {
+    //        switch (input)
+    //        {
+    //            case null: throw new ArgumentNullException(nameof(input));
+    //            case "": throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input));
+    //            default: return input[0].ToString().ToUpper() + input.Substring(1);
+    //        }
+    //    }
+    //}
 
 }
