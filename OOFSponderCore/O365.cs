56ae5a83d8881174491e917f133b49cc1e2d6f77
Evan Basalik <evanba@microsoft.com>
2025-02-17 07:48:15 -0500

    Code cleanup fixup

--- OOFSponderCore/O365.cs
+++ OOFSponderCore/O365.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
