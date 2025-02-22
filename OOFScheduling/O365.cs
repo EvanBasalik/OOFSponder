83bf2e1f0336046f11a24b80b25e1637d5519931
Evan Basalik <evanba@microsoft.com>
2025-02-22 12:54:35 -0500

    Missing Using

--- OOFScheduling/O365.cs
+++ OOFScheduling/O365.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
