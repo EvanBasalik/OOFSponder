943c600fad8418cb7a0e3b51cf08cbb73efeaaf5
Evan Basalik <evanba@microsoft.com>
2025-02-23 21:25:09 -0500

    Semi-working

--- OOFSponderCore/LastDateTimePicker.cs
+++ OOFSponderCore/LastDateTimePicker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace OOFScheduling
@@ -8,6 +9,7 @@ namespace OOFScheduling
     {
         public LastDateTimePicker()
         {
+            //this.SetStyle(ControlStyles.UserPaint, true);
         }
 
         //hooking into this allows us to preserve the old value
@@ -16,6 +18,7 @@ namespace OOFScheduling
             base.OnValueChanged(eventargs);
 
             LastValue = Value;
+
         }
 
         public DateTime LastValue { get; private set; }
@@ -27,6 +30,50 @@ namespace OOFScheduling
             {
                 base.Value = value;
             }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+
+            //int udWidth;
+            //// Find the up-down control within the DateTimePicker
+            //foreach (Control control in this.Controls)
+            //{
+            //    if (control is UpDownBase upDown)
+            //    {
+            //        // Adjust the width of the up-down control
+            //        udWidth = upDown.Width; // Set your desired width here
+            //        break;
+            //    }
+            //}
+
+            Graphics g = e.Graphics;
+            Rectangle rect = this.ClientRectangle;
+
+            // Draw the background
+            //g.FillRectangle(new SolidBrush(this.BackColor), rect);
+            //g.FillRectangle(new SolidBrush(System.Drawing.ColorTranslator.FromHtml("#F0F0F0")), rect);
+
+            // Draw the text
+            string text = this.Text;
+            // Create a StringFormat object to specify text alignment
+            StringFormat stringFormat = new StringFormat
+            {
+                Alignment = StringAlignment.Center, // Center horizontally
+                LineAlignment = StringAlignment.Center // Center vertically
+            };
+            using (Font font = new Font(this.Font.FontFamily, this.Font.Size, this.Font.Style))
+            {
+                SizeF textSize = g.MeasureString(text, font);
+                PointF location = new PointF((float)(rect.Width - textSize.Width * 1.35) / 2, (rect.Height - textSize.Height) / 2);
+                g.DrawString(text, font, new SolidBrush(this.ForeColor), location);
+                //g.DrawString(text, font, new SolidBrush(this.ForeColor), rect, stringFormat);
+            }
+
+
+            // Draw the drop-down button
+            Rectangle buttonRect = new Rectangle(rect.Width - 17, 0, 17, rect.Height);
+            ControlPaint.DrawComboButton(g, buttonRect, ButtonState.Normal);
         }
     }
 }
